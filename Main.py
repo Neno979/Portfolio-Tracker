5cb92587738f8ef9b3baa17e90e3ab1f9d0f2e4d
Neno979 <dr.nenica@gmail.com>
2026-02-13 00:26:30 +0100

    added routes to login and create acc page and linked with buttons

--- Main.py
+++ Main.py
@@ -2,14 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     headers = ["#", "Coin", "Price", "M.Cap.", "24hVol.", "24h%"]
-    return render_template('index.html', headers = headers)
+    return render_template("index.html", headers = headers)
 
-@app.route('/portfolio')
+@app.route("/portfolio")
 def portfolio():
     headers = ["Coin","share", "profit", "value", "size", "ma 7d"]
-    return render_template('portfolio.html', headers = headers)
-if __name__ == '__main__':
+    return render_template("portfolio.html", headers = headers)
+
+@app.route("/sign-in")
+def sign_in():
+    return render_template("signin.html")
+
+@app.route("/sign-up")
+def sign_up():
+    return render_template("signup.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
+
