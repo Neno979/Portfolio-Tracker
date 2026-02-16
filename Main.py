3b9f8bfc426bc80ecc0ba37d1e5bc8678d60c090
Neno979 <dr.nenica@gmail.com>
2026-02-16 22:53:07 +0100

    added get objects for signup form

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -7,19 +7,26 @@ def index():
     headers = ["#", "Coin", "Price", "M.Cap.", "24hVol.", "24h%"]
     return render_template("index.html", headers = headers)
 
-@app.route("/portfolio")
+@app.route("/portfolio" , methods=["GET", "POST"])
 def portfolio():
     headers = ["Coin","share", "profit", "value", "size", "ma 7d"]
     return render_template("portfolio.html", headers = headers)
 
-@app.route("/sign-in")
+@app.route("/sign-in" , methods=["GET", "POST"])
 def sign_in():
     return render_template("signin.html")
 
-@app.route("/sign-up")
+@app.route("/sign-up" , methods=["GET", "POST"])
 def sign_up():
-    return render_template("signup.html")
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        username = request.form.get("input_username")
+        email = request.form.get("input_email")
+        password = request.form.get("input_password")
+        confirm = request.form.get("confirm_password")
+        print("{0} : {1} : {2} : {3} ".format(username, email, password, confirm))
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
