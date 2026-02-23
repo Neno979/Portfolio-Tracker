412cc2f19baa26e32ff2140e410a3e1b2a7f88f3
Neno979 <dr.nenica@gmail.com>
2026-02-23 22:41:00 +0100

    started to pull data for 1 coin from API to database and filling table columns from database

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ("sqlite:///sqlite.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.db"
 db = SQLAlchemy(app)
 
 class Ptracker(db.Model):
@@ -15,18 +16,54 @@ class Ptracker(db.Model):
     password = db.Column(db.String, unique=True)
     confirm = db.Column(db.String, unique=True)
 
+class Coin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rank = db.Column(db.Integer, unique=True)
+    name = db.Column(db.String, unique=True)
+    price = db.Column(db.Float, unique=False)
+    mcap = db.Column(db.Float, unique=False)
+    volume = db.Column(db.Float, unique=False)
+    change = db.Column(db.Float, unique=False)
+
+
 with app.app_context():
     db.create_all()
 
 @app.route("/")
 def index():
-    headers = ["#", "Price", "M.Cap.", "24hVol.", "24h%"]
-    return render_template("index.html", headers = headers)
+    theads = ["#", "Price", "M.Cap.", "24hVol.", "24h%"]
+    API_KEY = "coinranking6ae704e6e7974096325d95be12cd9c383994de673acb79bf"
+    url = "https://api.coinranking.com/v2/coin/Qwsogvtv82FCd"
+    headers = {"x-access-token": API_KEY}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    bitcoin_data = data['data']['coin']
+    btc = Coin.query.filter_by(name='Bitcoin').first()
+
+    if btc:
+        btc.price = float(bitcoin_data['price'])
+        btc.mcap = float(bitcoin_data['marketCap'])
+        btc.volume = float(bitcoin_data['24hVolume'])
+        btc.change = float(bitcoin_data['change'])
+    else:
+        btc = Coin(
+            rank=1,
+            name=bitcoin_data['name'],
+            price=float(bitcoin_data['price']),
+            mcap=float(bitcoin_data['marketCap']),
+            volume=float(bitcoin_data['24hVolume']),
+            change=float(bitcoin_data['change'])
+        )
+        db.session.add(btc)
+
+    db.session.commit()
+
+    return render_template("index.html", headers = headers, theads = theads, btc = btc)
 
 @app.route("/portfolio" , methods=["GET", "POST"])
 def portfolio():
-    headers = ["Coin","share", "profit", "value", "size", "ma 7d"]
-    return render_template("portfolio.html", headers = headers)
+    theads = ["Coin","share", "profit", "value", "size", "ma 7d"]
+    return render_template("portfolio.html", theads = theads)
 
 @app.route("/sign-in" , methods=["GET", "POST"])
 def sign_in():
