9c194638155a71ca5d3392fd2f7317a51988d3be
Neno979 <dr.nenica@gmail.com>
2026-03-15 14:59:23 +0100

    added toggle button for password text visibility on signin/signup

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+function togglePassword(inputId, iconId) {
+    const input = document.getElementById(inputId);
+    const icon = document.getElementById(iconId);
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.className = "bi bi-eye-slash";
+    } else {
+        input.type = "password";
+        icon.className = "bi bi-eye";
+    }
+}
